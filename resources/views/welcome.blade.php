a81c034c2af80cedc490311c096afeed6fc537d8
jiro.kirameki <ec2-user@ip-172-31-10-198.us-east-2.compute.internal>
2018-06-05 06:31:44 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>welcome to Task-list</h1>
+             {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Route::resource('tasks', 'TasksController');
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
